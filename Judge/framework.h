f1d775c62ff9417a3177563a83a9ae38707d7c06
HYDRA15 <1048946361@qq.com>
2025-08-13 10:59:18 +0800

    update: JudgeLib

---
+++ Judge/framework.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // 从 Windows 头文件中排除极少使用的内容
+
+
+// 常用类型定义
+#define StaticString static constexpr std::string_view
+#define iExceptionCode static constexpr unsigned int
---
+++ Judge/iException.h
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include "framework.h"
+
+#include "Foundation/iExceptionBase.h"
+#include "Foundation/Infomation.h"
+
+namespace HYDRA15::Frameworks::Judge
+{
+    // 完整的 iException 类，相比基类额外记录了处理流程中所需要的信息
+    class iException : public HYDRA15::Frameworks::Foundation::iExceptionBase
+    {
+        Foundation::Infomation procInfo;
+        const std::exception innerExpt;
+
+    public:
+        // 构造和析构
+        iException() noexcept = delete;
+        iException(
+            const std::string& desp, 
+            const Foundation::Infomation& info = {},
+            const std::exception& inner = std::exception()
+        ) noexcept;
+        virtual ~iException() noexcept = default;
+
+        // 获取和写入处理信息
+        Foundation::Infomation& getProcInfo() const noexcept;
+        std::exception& getInnerExpt() const noexcept;
+    };
+}
