cf53ced08ab7dd39e28712befdb946c49ba6963b
HYDRA15 <1048946361@qq.com>
2025-08-24 00:02:36 +0800

    update

--- Foundation/Entry.cpp
+++ Foundation/Entry.cpp
@@ -3,25 +3,5 @@
 
 namespace HYDRA15::Foundation::Archivist
 {
-    std::string EntryBase::info() const
-    {
-        return visualize.unknownTypeInfoFormat.data();
-    }
 
-    Entry::Entry(const Entry& other)
-        :pImpl(other.pImpl ? other.pImpl->clone() : nullptr)
-    {
-    }
-
-    Entry::Entry(Entry&& other) 
-        :pImpl(std::move(other.pImpl))
-    {
-    }
-
-    std::string Entry::info() const
-    {
-        if (!pImpl)
-            return visualize.emptyInfo.data();
-        return pImpl->info();
-    }
 }
